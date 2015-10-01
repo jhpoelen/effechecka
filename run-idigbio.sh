6df16f7358bd391e5fce79be839cc1d018ab02ce
Jorrit Poelen <jhpoelen@gmail.com>
2015-09-30 17:31:27 -0700

    update job version

--- run-idigbio.sh
+++ run-idigbio.sh
@@ -2,5 +2,5 @@ nohup sbt run \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/idigbio/ \
 -Deffechecka.spark.master.url=spark://c18node15:6066 \
--Deffechecka.spark.job.jar=/home/int/idigbio-spark/target/scala-2.10/iDigBio-LD-assembly-1.0.jar \
+-Deffechecka.spark.job.jar=/home/int/idigbio-spark/target/scala-2.10/iDigBio-LD-assembly-1.1.jar \
 &> /home/int/effechecka/target/effechecka.log &

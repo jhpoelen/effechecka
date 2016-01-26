0635af449ba596ed667b56257be134f360cdf130
Jorrit Poelen <jhpoelen@gmail.com>
2016-01-25 18:58:37 -0800

    marathon testing

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sbt run \
--Deffechecka.host=0.0.0.0 \
--Deffechecka.data.dir=/home/int/data/idigbio/ \
--Deffechecka.spark.master.url=spark://c18node15:6066 \
--Deffechecka.spark.job.jar=/home/int/idigbio-spark/target/scala-2.10/iDigBio-LD-assembly-1.1.jar
+while [ true ] ; do echo 'Hello Marathon' ; sleep 5 ; done

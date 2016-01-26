9a28934edaf005a6a981447d76a83662645230a1
Jorrit Poelen <jhpoelen@gmail.com>
2016-01-26 15:11:32 -0800

    config

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 sbt run \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/idigbio/ \
--Deffechecka.spark.master.url=http://127.0.1.1:7077 \
+-Deffechecka.spark.master.url=mesos://apihack-c18.idigbio.org:7077 \
 -Deffechecka.spark.job.jar=/home/int/idigbio-spark/target/scala-2.10/iDigBio-LD-assembly-1.1.jar

b71156826c2bc326edb23a434e19f85e52ae015c
Jorrit Poelen <jhpoelen@gmail.com>
2016-01-26 13:59:28 -0800

    add mesos config for run

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 sbt run \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/idigbio/ \
--Deffechecka.spark.master.url=spark://c18node15:6066 \
+-Deffechecka.spark.master.url=mesos://127.0.1.1:5050 \
 -Deffechecka.spark.job.jar=/home/int/idigbio-spark/target/scala-2.10/iDigBio-LD-assembly-1.1.jar

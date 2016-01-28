2eccc1df8a5f9813f5e316caed8285f67df8f638
Jorrit Poelen <jhpoelen@gmail.com>
2016-01-27 20:44:38 -0800

    move schema check to config

--- src/main/scala/effechecka/Configure.scala
+++ src/main/scala/effechecka/Configure.scala
@@ -9,6 +9,10 @@ trait Configure {
   def session: Session = {
     val cluster = Cluster.builder()
       .addContactPoint(config.getString("effechecka.cassandra.host")).build()
+    val clusterSession = cluster.connect()
+    clusterSession.execute("CREATE KEYSPACE IF NOT EXISTS effechecka WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 }")
+    clusterSession.execute("CREATE TABLE IF NOT EXISTS effechecka.checklist (taxonselector TEXT, wktstring TEXT, traitselector TEXT, taxon TEXT, recordcount int, PRIMARY KEY((taxonselector, wktstring, traitselector), recordcount, taxon))")
+    clusterSession.execute("CREATE TABLE IF NOT EXISTS effechecka.checklist_registry (taxonselector TEXT, wktstring TEXT, traitselector TEXT, status TEXT, recordcount int, PRIMARY KEY(taxonselector, wktstring, traitselector))")
     cluster.connect("effechecka")
   }
 

8eb329165ed9d620c05875f594dd9c41bde96a9d
Jorrit Poelen <jhpoelen@gmail.com>
2016-03-07 10:26:36 -0800

    upgrade idigbio-spark job jar

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sbt run \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/ \
 -Deffechecka.spark.master.url=mesos://apihack-c18.idigbio.org:7077 \
--Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.4.0.jar
+-Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.4.1.jar

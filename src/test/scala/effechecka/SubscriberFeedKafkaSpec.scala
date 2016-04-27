95ddf15e04de468d28c59a7f4669c1a2f177d958
Jorrit Poelen <jhpoelen@gmail.com>
2016-04-27 08:51:57 -0700

    extend kafka test to include test consumer

--- src/test/scala/effechecka/SubscriberFeedKafkaSpec.scala
+++ src/test/scala/effechecka/SubscriberFeedKafkaSpec.scala
@@ -8,15 +8,18 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.Producer
+import akka.kafka.{ConsumerSettings, ProducerSettings}
+import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Source, Sink}
+import akka.stream.scaladsl.{Keep, Source, Sink}
+import akka.stream.testkit.TestSubscriber
+import akka.stream.testkit.scaladsl.TestSink
 import akka.stream.testkit.TestPublisher.Probe
 import akka.stream.testkit.scaladsl.TestSource
 import akka.testkit.TestKit
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerConfig}
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.scalactic.ConversionCheckedTripleEquals
 import org.scalatest._
 import scala.concurrent.duration._
@@ -31,6 +34,8 @@ class SubscriberFeedKafkaSpec extends TestKit(ActorSystem("KafkaIntegrationSpec"
   def uuid = UUID.randomUUID().toString
 
   var topic: String = _
+  var groupId: String = _
+  var clientId: String = _
 
   override def afterAll(): Unit = {
     shutdown(system, 30.seconds)
@@ -39,21 +44,42 @@ class SubscriberFeedKafkaSpec extends TestKit(ActorSystem("KafkaIntegrationSpec"
 
   override def beforeEach(): Unit = {
     topic = "test-topic-" + uuid
+    groupId = "test-group-" + uuid
+    clientId = "test-client-" + uuid
   }
+
+  val initialMsg = "some initial test message"
 
   "send record to topic" in {
     val settings = ProducerSettings(system, new StringSerializer, new StringSerializer)
       .withBootstrapServers("localhost:9092")
 
     val producer = settings.createKafkaProducer()
-    producer.send(new ProducerRecord(topic, "some test message"))
+    producer.send(new ProducerRecord(topic, initialMsg))
     producer.close(60, TimeUnit.SECONDS)
 
     val event = SelectorSubscriptionEvent(OccurrenceSelector("taxa", "wkt", "traits"), new URL("mailto:foo@bar"))
 
-    val probe: Source[ProducerRecord[String, String], Probe[ProducerRecord[String, String]]] = TestSource.probe[ProducerRecord[String, String]]
-    probe.runWith(Producer.plainSink(settings))
+    val testSource: Source[ProducerRecord[String, String], Probe[ProducerRecord[String, String]]] = TestSource.probe[ProducerRecord[String, String]]
+    val (probe, notUsed) = testSource.toMat(Producer.plainSink(settings))(Keep.both).run()
 
+    val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer, Set(topic))
+      .withGroupId(groupId)
+      .withClientId(clientId)
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+      .withBootstrapServers("localhost:9092")
 
+    val subscriber = Consumer.plainSource(consumerSettings)
+      .filterNot(_.value == initialMsg)
+      .map(_.value)
+      .runWith(TestSink.probe)
+
+    probe.sendNext(new ProducerRecord(topic, "some other test message"))
+
+    subscriber
+      .request(1)
+      .expectNext("some other test message")
+
+    subscriber.cancel()
   }
 }

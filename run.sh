7f9ef092815f79c2cd03e3c87193a70b5144032a
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-25 17:16:45 -0700

    upgrade spark job

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 sbt "run-main effechecka.WebApi" \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/ \
--Deffechecka.spark.master.url=mesos://apihack-c18.idigbio.org:7077 \
--Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.5.2.jar
+-Deffechecka.spark.master.url=mesos://api.effechecka.org:7077 \
+-Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.5.4.jar

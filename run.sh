8be33d7daf0a941d838dfad12c3c71cada566596
jhpoelen <jhpoelen@xs4all.nl>
2017-05-19 11:04:54 -0700

    switch to using [latest] job version

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sbt "run-main effechecka.WebApi" \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.data.dir=/home/int/data/ \
 -Deffechecka.spark.master.url=mesos://api.effechecka.org:7077 \
--Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.5.6.jar
+-Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-latest.jar

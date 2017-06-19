b04022b1719bf74cfa4a5fced67a3d118147bed0
jhpoelen <jhpoelen@xs4all.nl>
2017-06-19 16:13:34 -0700

    add new config

--- run-hdfs.sh
+++ run-hdfs.sh
@@ -2,6 +2,6 @@
 sbt "run-main effechecka.WebApi" \
 -Deffechecka.host=0.0.0.0 \
 -Deffechecka.port=8889 \
--Deffechecka.data.dir=hdfs://mesos02.acis.ufl.edu/guoda/data/ \
--Deffechecka.spark.master.url=mesos://api.effechecka.org:7077 \
--Deffechecka.spark.job.jar=/home/int/jobs/iDigBio-LD-assembly-1.5.5.jar
+-Deffechecka.data.dir=hdfs:/guoda/data/gbif-idigbio.parquet \
+-Deffechecka.spark.master.url=mesos://mesos07.acis.ufl.edu:7077 \
+-Deffechecka.spark.job.jar=hdfs:/guoda/lib/iDigBio-LD-assembly-latest.jar

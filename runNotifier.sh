57ebe5c672daf28269fefe65a80e06c662c175cf
jhpoelen <jhpoelen@xs4all.nl>
2017-06-20 10:25:09 -0700

    remove unused scripts

--- runNotifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]; then
-  echo "usage: $0 [mailgun api key]";
-  echo " ";
-  exit 1;
-fi
-
-sbt "run-main effechecka.SubscriptionNotifier" \
--Deffechecka.mailgun.apikey=$1

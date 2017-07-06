9ecb13fd9bdea8a7dbcd78329c5daee6a86d3f83
jhpoelen <jhpoelen@xs4all.nl>
2017-07-06 16:16:23 -0700

    fix test error

--- src/test/scala/effechecka/SparkSubmitterSpec.scala
+++ src/test/scala/effechecka/SparkSubmitterSpec.scala
@@ -32,7 +32,7 @@ class SparkSubmitterSpec extends TestKit(ActorSystem("SparkIntegrationTest"))
     val expectedRequestBody =
       """{
         |      "action" : "CreateSubmissionRequest",
-        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///some/local/path/monitors\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "\"Animalia|Insecta\"", "\"ENVELOPE(-150,-50,40,10)\"", "\"\""],
+        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///tmp/monitor\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "\"Animalia|Insecta\"", "\"ENVELOPE(-150,-50,40,10)\"", "\"\""],
         |      "appResource" : "file:///doesnotexist.jar",
         |      "clientSparkVersion" : "2.0.1",
         |      "environmentVariables" : {
@@ -66,7 +66,7 @@ class SparkSubmitterSpec extends TestKit(ActorSystem("SparkIntegrationTest"))
     val expectedRequestBody =
       """{
         |      "action" : "CreateSubmissionRequest",
-        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///some/local/path/monitors\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "\"Animalia|Insecta\"", "\"ENVELOPE(-150,-50,40,10)\"", "\"\""],
+        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///tmp/monitor\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "\"Animalia|Insecta\"", "\"ENVELOPE(-150,-50,40,10)\"", "\"\""],
         |      "appResource" : "file:///doesnotexist.jar",
         |      "clientSparkVersion" : "2.0.1",
         |      "environmentVariables" : {
@@ -99,7 +99,7 @@ class SparkSubmitterSpec extends TestKit(ActorSystem("SparkIntegrationTest"))
     val expectedRequestBody =
       """{
         |      "action" : "CreateSubmissionRequest",
-        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///some/local/path/monitors\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "-a", "true"],
+        |      "appArgs" : [ "-f", "hdfs","-o", "\"file:///tmp/monitor\"","-c","\"file:///does/not/exist/gbif-idigbio.parquet\"","-t", "\"file:///does/not/exist/traitbank/*.csv\"", "-a", "true"],
         |      "appResource" : "file:///doesnotexist.jar",
         |      "clientSparkVersion" : "2.0.1",
         |      "environmentVariables" : {
